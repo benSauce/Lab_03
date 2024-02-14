d57908e4a7720135f0aa48601bf334e25ef6f9a3
bsauce <bsauce11@calstatela.edu>
2024-02-14 13:15:56 -0800

    removeDuplicates works with Integer Class

--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
-    }
-}
+
+        // Get the ArrayList with duplicate values
+        ArrayList<Integer> list = new ArrayList<>( Arrays.asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
+
+        // Print the Arraylist
+        System.out.println("ArrayList with duplicates: " + list);
+
+        // Remove duplicates
+        ArrayList<Integer> newList = removeDuplicates(list);
+
+        // Print the ArrayList with duplicates removed
+        System.out.println("ArrayList with duplicates removed: " + newList);
+    } // End of main()
+
+    public static <E> ArrayList<E> removeDuplicates (ArrayList<E> list){
+        // Create a new ArrayList
+        ArrayList<E> newList = new ArrayList<E>();
+
+        // Traverse through the first list
+        for (E element : list) {
+            // If this element is not present in newList then add it
+            if (!newList.contains(element)) {
+                newList.add(element);
+            } //End if
+        } //End for
+        return newList;
+
+    } //End of removeDuplicates()
+} // End of Main

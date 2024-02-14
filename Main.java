4327a99a840219c9f67c9564a8afd3427771d627
bsauce <bsauce11@calstatela.edu>
2024-02-14 13:20:04 -0800

    removeDuplicates works with Integer and Confirmed works with String also. Generic implementation confirmed.

--- Main.java
+++ Main.java
@@ -7,13 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         // Get the ArrayList with duplicate values
-        ArrayList<Integer> list = new ArrayList<>( Arrays.asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
+        //ArrayList<Integer> list = new ArrayList<>( Arrays.asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
+        ArrayList<String> list = new ArrayList<>( Arrays.asList("cat","dog","bat","rat","dog","bat","cat"));
 
         // Print the Arraylist
         System.out.println("ArrayList with duplicates: " + list);
 
         // Remove duplicates
-        ArrayList<Integer> newList = removeDuplicates(list);
+        //ArrayList<Integer> newList = removeDuplicates(list);
+        ArrayList<String> newList = removeDuplicates(list);
+
 
         // Print the ArrayList with duplicates removed
         System.out.println("ArrayList with duplicates removed: " + newList);

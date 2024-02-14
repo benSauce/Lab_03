9d80c4bf758ad624f423de56db5980efb1689f61
bsauce <bsauce11@calstatela.edu>
2024-02-14 13:34:52 -0800

    Started pt 2 LinSearch

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        // Print the ArrayList with duplicates removed
+        System.out.println("Beginning Pt 2, Linear Search");
+    } // End of main()
+
+    public static <E extends Comparable<E>> int linearSearch(E[] list, E key){
+
+        return 0;
+    } //End of linearSearch()
+} // End of Main

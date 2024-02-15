0a557557dfd06ab8fc343adf0a2d8f3b223703c8
Benja Saucedo <benjaminsaucedo7@gmail.com>
2024-02-14 20:28:21 -0800

    Completed Generic Linear Search Class

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,16 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class LinearSearch {
 
     public static void main(String[] args) {
 
-        // Print the ArrayList with duplicates removed
-        System.out.println("Beginning Pt 2, Linear Search");
+        //driver code to test linear search method
+        //Integer arr[] = {2, 3, 4, 7, 9, 10, 40};
+        //Integer x = 10;
+        String[] arr = {"cat","dog","bat","rat","fog","nat","matt"};
+        String x = "matt";
+
+        //Call function
+        int result = linearSearch(arr, x);
+        if (result == -1){
+            System.out.print( "Element is not present in Array.");
+        } else {
+            System.out.print("Element is present at index " + result);
+        }
     } // End of main()
 
     public static <E extends Comparable<E>> int linearSearch(E[] list, E key){
-
-        return 0;
+        //for loop to to parse through array
+        for (int i = 0; i < list.length; i++){
+            //check to see if data at index matches key
+            if (list[i] == key)
+                //return index of match
+                return i;
+        }
+        //else return -1
+        return -1;
     } //End of linearSearch()
 } // End of Main

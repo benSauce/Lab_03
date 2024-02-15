8ad062a1129cfa1c350b0cd229c1c1806ecfdd49
Benja Saucedo <benjaminsaucedo7@gmail.com>
2024-02-14 23:17:08 -0800

    Starting sort()

---
+++ SortGeneric.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SortGeneric {
+
+    public static void main(String[] args) {
+
+
+        // Print the ArrayList with duplicates removed
+        System.out.println("Sup broski");
+    } // End of main()
+    public static <E extends Comparable<E>> void sort(E[] list){
+
+    }//end sort()
+} // end SortGeneric
